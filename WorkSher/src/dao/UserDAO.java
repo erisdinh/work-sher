b5d34d6f17b6bf26001e9c72e270ea6a883befc1
abby <annairamos@gmail.com>
2018-11-29 17:22:25 -0500

    Updated DBUtil for connectivity, implemented user registration

---
+++ WorkSher/src/dao/UserDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.User;
+import util.DBUtil;
+
+public class UserDAO {
+	
+	// Method to add the user to the database
+	// TESTED - This works
+	public static void addUser(User user) {
+		Connection conn = null;		
+		try {
+			conn = DBUtil.getConnection();
+			PreparedStatement pStmt = conn.prepareStatement("insert into usertest" 
+														+ "(username, user_pass, name, email, date_joined, role)"
+														+ " values (?, ?, ?, ?, ?, ? )");					
+			pStmt.setString(1, user.getUsername());
+			pStmt.setString(2, user.getPassword());
+			pStmt.setString(3, user.getName());
+			pStmt.setString(4, user.getEmail());
+			pStmt.setDate(5, new java.sql.Date (user.getDateJoined().getTime()));
+			pStmt.setString(6, user.getRole());
+			pStmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.closeConnection(conn);
+		}
+	}
+}
--- WorkSher/src/util/DBUtil.java
+++ WorkSher/src/util/DBUtil.java
@@ -1,5 +1,32 @@
 package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtil {
+	private static Connection conn = null;
+	private static String dbURL = "jdbc:mysql://localhost:3306/worksher_db";
+	private static String username = "root";
+	private static String password = "1234";
 
+	// Method to get the database connection
+	public static Connection getConnection() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	// Method to close the database connection
+	public static void closeConnection(Connection conn) {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
 }
