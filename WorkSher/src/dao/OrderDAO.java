044c3a6027c99909bfa3c3fa8a7bc905d9ba6f7b
erisdinh <erisdinh@gmail.com>
2018-12-02 18:45:13 -0500

    Created order componets and added blank methods in UserDAO and PostingDAO (getById)

---
+++ WorkSher/src/dao/OrderDAO.java
@@ -0,0 +1,134 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.DBUtil;
+import model.*;
+
+import java.util.ArrayList;
+
+public class OrderDAO {
+
+	public static void addOrder(Order order) throws SQLException {
+		
+		Connection connection = null;
+		
+		try {
+			connection = DBUtil.getConnection();
+			
+			PreparedStatement pstmt = connection.prepareStatement("insert into orders (requestOrderUser_id, postOrderUser_id, posting_id, dateRequested, dateResponsed, dateCompleted, status) values"
+					+ "(?, ?, ?, ?, ?, ?, ?);");
+			pstmt.setInt(1, order.getRequestUser().getUserid());
+			pstmt.setInt(2, order.getPostUser().getUserid());
+			pstmt.setInt(3, order.getPosting().getPostingid());
+			pstmt.setDate(4, order.getDateRequested());
+			pstmt.setDate(5, order.getDateResponsed());
+			pstmt.setDate(6, order.getDateCompleted());
+			pstmt.setString(7, order.getStatus());
+			
+			pstmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			e.getMessage();
+		} finally {
+			connection.close();
+		}
+	}
+	
+	public static ArrayList<Order> getAllOrder() throws SQLException {
+		
+		Connection connection = null;
+		ArrayList<Order> allOrder = new ArrayList<>();
+		
+		try {
+			connection = DBUtil.getConnection();
+			
+			Statement stmt = connection.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("select * from orders");
+			
+			while(rs.next()) {
+				
+				Order order = new Order();
+				
+				order.setOrderid(rs.getInt("order_id"));
+				
+				int requestUserId = rs.getInt("requestOrderUser_id");
+				User requestUser = UserDAO.getUserById(requestUserId);
+				order.setRequestUser(requestUser);
+				
+				int postUserId = rs.getInt("postOrderUser_id");
+				User postUser = UserDAO.getUserById(postUserId);
+				order.setPostUser(postUser);
+				
+				int postingid = rs.getInt("posting_id");
+				Posting posting = PostingDAO.getPostingById(postingid);
+				order.setPosting(posting);
+				
+				order.setDateRequested(rs.getDate("dateRequested"));
+				order.setDateResponsed(rs.getDate("dateResponsed"));
+				order.setDateCompleted(rs.getDate("dateCompleted"));
+				order.setStatus(rs.getString("status"));
+				
+				
+				allOrder.add(order);
+			}
+			
+		} catch(SQLException e) {
+			e.getMessage();
+		} finally {
+			connection.close();
+		}
+		
+		return allOrder;
+	}
+	
+	public static Order getOrderById(int orderid) throws SQLException {
+		
+		Connection connection = null;
+		Order order = new Order();
+		
+		try {
+			
+			connection = DBUtil.getConnection();
+			
+			PreparedStatement pstmt = connection.prepareStatement("select * from orders where order_id = ?");
+			
+			pstmt.setInt(1, orderid);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				order.setOrderid(rs.getInt("order_id"));
+				
+				int requestUserId = rs.getInt("requestOrderUser_id");
+				User requestUser = UserDAO.getUserById(requestUserId);
+				order.setRequestUser(requestUser);
+				
+				int postUserId = rs.getInt("postOrderUser_id");
+				User postUser = UserDAO.getUserById(postUserId);
+				order.setPostUser(postUser);
+				
+				int postingid = rs.getInt("posting_id");
+				Posting posting = PostingDAO.getPostingById(postingid);
+				order.setPosting(posting);
+				
+				order.setDateRequested(rs.getDate("dateRequested"));
+				order.setDateResponsed(rs.getDate("dateResponsed"));
+				order.setDateCompleted(rs.getDate("dateCompleted"));
+				order.setStatus(rs.getString("status"));
+			}
+			
+		} catch(SQLException e) {
+			
+		} finally {
+			connection.close();
+		}
+		
+		return order;
+	}
+}
---
+++ WorkSher/src/model/Order.java
@@ -0,0 +1,93 @@
+package model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Order implements Serializable{
+	
+	private int orderid;
+	private User requestUser;
+	private User postUser;
+	private Posting posting;
+	private Date dateRequested;
+	private Date dateResponsed;
+	private Date dateCompleted;
+	private String status;
+	
+	public Order() {}
+	
+	public Order(int orderid, User requestUser, User postUser, Posting posting, Date dateRequested, Date dateResponsed, Date dateCompleted, String status) {
+		this.orderid = orderid;
+		this.requestUser = requestUser;
+		this.postUser = postUser;
+		this.posting = posting;
+		this.dateRequested = dateRequested;
+		this.dateResponsed = dateResponsed;
+		this.dateCompleted = dateCompleted;
+		this.status = status;
+	}
+
+	public int getOrderid() {
+		return orderid;
+	}
+
+	public void setOrderid(int orderid) {
+		this.orderid = orderid;
+	}
+
+	public User getRequestUser() {
+		return requestUser;
+	}
+
+	public void setRequestUser(User requestUser) {
+		this.requestUser = requestUser;
+	}
+
+	public User getPostUser() {
+		return postUser;
+	}
+
+	public void setPostUser(User postUser) {
+		this.postUser = postUser;
+	}
+
+	public Posting getPosting() {
+		return posting;
+	}
+
+	public void setPosting(Posting posting) {
+		this.posting = posting;
+	}
+
+	public Date getDateRequested() {
+		return dateRequested;
+	}
+
+	public void setDateRequested(Date dateRequested) {
+		this.dateRequested = dateRequested;
+	}
+
+	public Date getDateResponsed() {
+		return dateResponsed;
+	}
+
+	public void setDateResponsed(Date dateResponsed) {
+		this.dateResponsed = dateResponsed;
+	}
+
+	public Date getDateCompleted() {
+		return dateCompleted;
+	}
+
+	public void setDateCompleted(Date dateCompleted) {
+		this.dateCompleted = dateCompleted;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
