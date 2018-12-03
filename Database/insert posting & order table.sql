d294981bc25c67a984de6c7915c994a4de6cc3d6
erisdinh <erisdinh@gmail.com>
2018-12-02 21:10:30 -0500

    fixed orders table with automatic requestedDate

--- Database/insert posting & order table.sql
+++ Database/insert posting & order table.sql
@@ -7,6 +7,6 @@ insert into posting (user_id, jobCategory, description, compensation, status, po
 (2, 'Design', 'I can design posters', '2 cups of coffee', 'active', '', '2018-11-29 00:00:00'),
 (1, 'Photography', 'Hi every one!,\n, I am a second-year photography student.', 'Photography posters', 'inactive','', '2018-11-29 00:00:00');
 
-insert into orders (requestOrderUser_id, postOrderUser_id, posting_id, description, dateRequested, dateResponsed, dateCompleted, status) values
-(1, 2, 1, '2 posters for Academic events', '2018-12-02 00:00:00', null, null, 'pending'), 
-(2, 1, 2, 'A yearbook for my class','2018-11-29 00:00:00', '2018-11-30 00:00:00', null, 'approved');
+insert into orders (requestOrderUser_id, postOrderUser_id, posting_id, description, dateResponsed, dateCompleted, status) values
+(1, 2, 1, '2 posters for Academic events', null, null, 'pending'), 
+(2, 1, 2, 'A yearbook for my class', '2018-11-30 00:00:00', null, 'approved');
